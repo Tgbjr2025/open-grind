5dea4b8cb0a3af22e091ab74a9cfeeffe65dae32
Viktor Shchelochkov <hi@hloth.dev>
2026-05-08 16:56:03 +0200

    Add sign out page for testing

---
+++ src/routes/auth/sign-out/+page@.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { callMethod } from "$lib/api";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		callMethod("logout")
+			.catch((e) => {
+				console.error(e);
+				return null;
+			})
+			.then(() => {
+				window.location.href = "/auth/sign-in";
+			});
+	});
+</script>
